7467b8f993bab36e64daa8bed9e63b6167ff8822
Jared Bunting <jared.bunting@digitalreasoning.com>
2011-02-10 13:15:56 -0600

    Added "simple" plugin - this just manages a cluster that has no specific service installed on it.  It can be used for a one-off cluster or as the basis of developing a new plugin.

---
+++ plugins/simple/cli.py
@@ -0,0 +1,126 @@
+import sys
+import logging
+import urllib
+
+from cloud.plugin import CLIPlugin
+from cloud.plugin import BASIC_OPTIONS
+from cloud.service import InstanceTemplate
+from optparse import make_option
+from prettytable import PrettyTable
+from pprint import pprint
+
+# Add options here to override what's in the clusters.cfg file
+# TODO
+
+class SimpleServiceCLI(CLIPlugin):
+    USAGE = """Simple service usage: CLUSTER COMMAND [OPTIONS]
+where COMMAND and [OPTIONS] may be one of:
+            
+                               CLUSTER COMMANDS
+  ----------------------------------------------------------------------------------
+  details                             list instances in CLUSTER
+  launch-cluster NUM_NODES            launch NUM_NODES Cassandra nodes
+  expand-cluster NUM_NODES            adds new nodes
+  terminate-cluster                   terminate all instances in CLUSTER
+  login                               log in to the master in CLUSTER over SSH
+"""
+#  transfer FILE DESTINATION           transfer a file to all nodes
+#  execute COMMAND                     execute a command on all nodes
+
+    def __init__(self):
+        super(SimpleServiceCLI, self).__init__()
+
+        #self._logger = logging.getLogger("CassandraServiceCLI")
+ 
+    def execute_command(self, argv, options_dict):
+        if len(argv) < 2:
+            self.print_help()
+
+        self._cluster_name = argv[0]
+        self._command_name = argv[1]
+
+        # strip off the cluster name and command from argv
+        argv = argv[2:]
+
+        # handle all known commands and error on an unknown command
+        if self._command_name == "details":
+            self.print_instances()
+
+        elif self._command_name == "terminate-cluster":
+            self.terminate_cluster(argv, options_dict)
+
+        elif self._command_name == "launch-cluster":
+            self.launch_cluster(argv, options_dict)
+
+        elif self._command_name == "expand-cluster":
+            self.expand_cluster(argv, options_dict)
+
+        elif self._command_name == "login":
+            self.login(argv, options_dict)
+
+        else:
+            self.print_help()
+
+    def expand_cluster(self, argv, options_dict):
+        expected_arguments = ["NUM_INSTANCES"]
+        opt, args = self.parse_options(self._command_name,
+                                       argv,
+                                       expected_arguments=expected_arguments,
+                                       unbounded_args=True)
+        opt.update(options_dict)
+
+        # check for the cassandra-specific files
+        if opt.get('cassandra_config_file') is None:
+            print "ERROR: No cassandra_config_file configured. Aborting."
+            sys.exit(1)
+
+        number_of_nodes = int(args[0])
+        instance_template = InstanceTemplate(
+            (self.service.SIMPLE_NODE,),
+            number_of_nodes,
+            opt.get('image_id'),
+            opt.get('instance_type'),
+            opt.get('key_name'),
+            opt.get('public_key'),
+            opt.get('user_data_file'),
+            opt.get('availability_zone'),
+            opt.get('user_packages'),
+            opt.get('auto_shutdown'),
+            opt.get('env'),
+            opt.get('security_groups'))
+#        instance_template.add_env_strings(["CLUSTER_SIZE=%d" % number_of_nodes])
+
+        print "Expanding cluster by %d instance(s)...please wait." % number_of_nodes
+
+        self.service.expand_cluster(instance_template,
+                                    opt.get('ssh_options'))
+
+    def launch_cluster(self, argv, options_dict):
+        """
+        """
+        expected_arguments = ["NUM_INSTANCES"]
+        opt, args = self.parse_options(self._command_name, 
+                                      argv,
+                                      expected_arguments=expected_arguments)
+        opt.update(options_dict)
+
+        number_of_nodes = int(args[0])
+        instance_template = InstanceTemplate(
+            (self.service.SIMPLE_NODE,),
+            number_of_nodes,
+            opt.get('image_id'),
+            opt.get('instance_type'),
+            opt.get('key_name'),
+            opt.get('public_key'), 
+            opt.get('user_data_file'),
+            opt.get('availability_zone'), 
+            opt.get('user_packages'),
+            opt.get('auto_shutdown'), 
+            opt.get('env'),
+            opt.get('security_groups'))
+        instance_template.add_env_strings(["CLUSTER_SIZE=%d" % number_of_nodes])
+
+        print "Launching cluster with %d instance(s)...please wait." % number_of_nodes
+
+        self.service.launch_cluster(instance_template,
+                                    opt.get('ssh_options'))
---
+++ plugins/simple/service.py
@@ -0,0 +1,87 @@
+import os
+import sys
+import time
+import subprocess
+import urllib
+import tempfile
+
+from cloud.cluster import TimeoutException
+from cloud.service import InstanceTemplate
+from cloud.plugin import ServicePlugin 
+from cloud.util import xstr
+
+from yaml import load as parse_yaml
+from yaml import dump as dump_yaml
+
+try:
+    from cElementTree import parse as parse_xml
+    from cElementTree import tostring as dump_xml
+    from cElementTree import Element
+except:
+    try:
+        from xml.etree.cElementTree import parse as parse_xml
+        from xml.etree.cElementTree import tostring as dump_xml
+        from xml.etree.cElementTree import Element
+    except:
+        print "*"*80
+        print "WARNING: cElementTree module does not exist. Defaulting to elementtree instead."
+        print "It's recommended that you install the cElementTree module for faster XML parsing."
+        print "*"*80
+        from elementtree.ElementTree import parse as parse_xml
+        from elementtree.ElementTree import parse as parse_xml
+        from elementtree.ElementTree import Element
+
+class SimpleService(ServicePlugin):
+    """
+    """
+    SIMPLE_NODE = "sn"
+
+    def __init__(self):
+        super(SimpleService, self).__init__()
+
+    def get_roles(self):
+        return [self.SIMPLE_NODE]
+
+    def get_instances(self):
+        return self.cluster.get_instances_in_role(self.SIMPLE_NODE, "running")
+
+    def expand_cluster(self, instance_template, ssh_options):
+        instances = self.get_instances()
+
+        instance_ids = self._launch_instances(instance_template)
+
+        if len(instance_ids) != instance_template.number:
+            self.logger.warn("Number of reported instance ids (%d) " \
+                             "does not match requested number (%d)" % \
+                             (len(instance_ids), instance_template.number))
+        self.logger.debug("Waiting for %s instance(s) to start: %s" % \
+            (instance_template.number, ", ".join(instance_ids)))
+        time.sleep(1)
+
+        try:
+            self.cluster.wait_for_instances(instance_ids)
+            self.logger.debug("%d instances started" % (instance_template.number,))
+        except TimeoutException:
+            self.logger.error("Timeout while waiting for %s instance to start." % \
+                ",".join(instance_template.roles))
+
+        instances = self.get_instances()
+        self.logger.debug("We have %d current instances...", len(instances))
+        new_instances = [instance for instance in instances if instance.id in instance_ids]
+        if(len(new_instances) != len(instance_ids)) :
+            raise Exception("Could only find %d new instances, expected %s" % (len(new_instances), str(instance_ids)))
+
+        self.logger.info("Instances started: %s" % (str(new_instances),))
+
+
+    def launch_cluster(self, instance_template, ssh_options):
+        """
+        """
+        if self.get_instances() :
+            raise Exception("This cluster is already running.  It must be terminated prior to being launched again.")
+
+        self.expand_cluster(instance_template, ssh_options)
+
+    def login(self, instance, ssh_options):
+        ssh_command = self._get_standard_ssh_command(instance, ssh_options)
+        subprocess.call(ssh_command, shell=True)
